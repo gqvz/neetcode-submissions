b213d158abe7a3b97b39a2a5dfea749054390169
Garvit Sharma <sharmagarvit614@gmail.com>
2026-04-12 04:44:05 +0530

    Remove: buy-and-sell-crypto - submission-13

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-13.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        m = r = 0
-        for i, n in enumerate(prices[::-1]):
-            m = max(m, n)
-            r = max(r, m - prices[~i])
-        return r
