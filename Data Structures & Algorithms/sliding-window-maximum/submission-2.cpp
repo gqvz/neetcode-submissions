403ebd8ad10f84b865fe7a6b4c6786bc799c1a33
Garvit Sharma <sharmagarvit614@gmail.com>
2026-04-25 04:14:57 +0530

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+#pragma GCC optimize("Ofast, unroll-loops")
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        multiset<int> cs;
+        for (int i = 0; i < k-1; i++){
+            cs.insert(nums[i]);
+        }
+
+        vector<int> res(nums.size() - k + 1);
+        for (int i = k-1; i < nums.size(); i++){
+            cs.insert(nums[i]);
+            res[i-k+1] = *cs.rbegin();
+            cs.erase(cs.find(nums[i-k+1]));
+        }
+        return res;
+    }
+};
