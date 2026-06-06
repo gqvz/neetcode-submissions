4df95b8a291867169dc73efa04d2e6a351b14950
Garvit Sharma <sharmagarvit614@gmail.com>
2026-06-06 08:22:28 +0530

    Add: jump-game-ii - submission-7

---
+++ Data Structures & Algorithms/jump-game-ii/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) < 2: return 0
+        if nums[0] >= len(nums): return 1
+        r = nums[0]
+        c = 1
+        cmr = r
+        for i, num in enumerate(nums):
+            if r < i: return False
+            if num + i > r:
+                r = num + i
+                print("moved r =", r)
+            if i == cmr and i != len(nums) - 1:
+                c += 1
+                cmr = r
+                print("moved cmr =", cmr, "n =", num, "i =", i)
+        return c
