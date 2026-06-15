698479957a2f56eb3551a99a2ca00207806bdf1c
Garvit Sharma <sharmagarvit614@gmail.com>
2026-06-15 16:44:33 +0530

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wds = set(wordDict)
+        m = {}
+        def dfs(s1):
+            if s1 in wds: 
+                m[s1] = True
+                return True
+            if s1 in m:
+                return m[s1]
+            for i in range(1, len(s1)+1):
+                if s1[:i] in wds:
+                    if dfs(s1[i:]):
+                        m[s1] = True
+                        return True
+            m[s1] = False
+        dfs(s)
+        return m[s]
