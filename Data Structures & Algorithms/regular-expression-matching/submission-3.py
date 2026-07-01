a74d206e8398dbef42ce51ccbb605c98f3157a6a
Garvit Sharma <sharmagarvit614@gmail.com>
2026-07-01 22:42:23 +0530

    Add: regular-expression-matching - submission-3

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-3.py
@@ -0,0 +1,18 @@
+from functools import cache
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        @cache
+        def dfs(i, j):
+            if j == len(p) and i == len(s): return True
+            if i == len(s): return p[-1] == '*'
+            if j == len(p): return False
+            if j == len(p)-1:
+                return (p[j] == '.' or p[j] == s[i]) and i == len(s)-1
+
+            if p[j+1] == '*':
+                if p[j] == '.':
+                    return dfs(i+1, j) or dfs(i, j+2)
+                else:
+                    return ((p[j] == s[i]) and dfs(i+1, j)) or dfs(i, j+2)
+            return (p[j] == '.' or p[j] == s[i]) and dfs(i+1, j+1)
+        return dfs(0, 0)
